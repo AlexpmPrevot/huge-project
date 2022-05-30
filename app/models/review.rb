004a60ac7d9c4c6d3fa5f31897c410423cbf0602
Alexandre Prevot <a.prevot33@gmail.com>
2022-05-30 17:42:04 +0200

    db crashtest  ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :target
-  belongs_to :reviewer
+  belongs_to :target, class_name: "User", foreign_key: :target_id
+  belongs_to :reviewer, class_name: "User", foreign_key: :reviewer_id
   belongs_to :hug
 end
