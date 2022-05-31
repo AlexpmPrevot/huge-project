25bc03d0999079d8d9083548b0e3aa2b5c7874e5
Alexandre Prevot <a.prevot33@gmail.com>
2022-05-31 17:08:20 +0200

    user index added last connexion or online status and button to map

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,12 @@ class UsersController < ApplicationController
     @markers = @users.geocoded.map do |user|
       {
         lat: user.latitude,
-        lng: user.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { user: user })
+        lng: user.longitude
       }
     end
   end
-    
-   def show
+
+  def show
     @user = User.find(params[:id])
-   end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :city
+  after_validation :geocode, if: :will_save_change_to_city?
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def currently_logged_in(logged_in = true)
+    where(logged_in: logged_in).where("last_request_at > ?", Time.zone.now - timeout_in.seconds)
+  end
+
+  def currently_logged_in?
+    logged_in? && last_request_at.present? && !timedout?(last_request_at)
+  end
 end
