a3f5bd0dca8a4a90cf10b9780abe5daab65d6c68
Destyka <102505452+Destyka@users.noreply.github.com>
2022-05-31 15:41:34 +0200

    correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :city
+  after_validation :geocode, if: :will_save_change_to_city?
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
