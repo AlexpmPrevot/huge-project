c3bf9226d5d0e9aa5155870d423efdcc433cf760
Alexandre Prevot <a.prevot33@gmail.com>
2022-05-31 15:08:08 +0200

    user index cards done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
   devise_for :users
   root to: 'pages#home'
   resources :hugs do
     resources :reviews, only: %i[new create]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[show index]
 end
