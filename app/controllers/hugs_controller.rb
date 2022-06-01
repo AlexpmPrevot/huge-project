6f961e32e4f590d4641affc443178b22f652177d
Cesar Victor <cesarvictor@hotmail.fr>
2022-06-01 11:19:40 +0200

    hugs contoller

--- app/controllers/hugs_controller.rb
+++ app/controllers/hugs_controller.rb
@@ -1,2 +1,30 @@
 class HugsController < ApplicationController
+  def new
+    @hug = Hug.new
+  end
+
+  def show
+    @hug = Hug.find(params[:id])
+  end
+
+  def create
+    @hug = Hug.new(hug_params)
+    if @hug.save
+      redirect_to @hug
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @hug = Hug.find(params[:id])
+    @hug.update(hug_params)
+    redirect_to @hug
+  end
+
+  private
+
+  def hug_params
+    params.require(:hug).permit(:progress, :receiver_id, :sender_id)
+  end
 end
