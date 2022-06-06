d167da45d85f83e316ff45c7c2b121e5324dcd40
Alexandre Prevot <a.prevot33@gmail.com>
2022-06-06 12:59:25 +0200

    geoloc working refresh 3sec

--- app/javascript/controllers/geoloc_controller.js
+++ app/javascript/controllers/geoloc_controller.js
@@ -8,14 +8,14 @@ export default class extends Controller {
   }
 
   connect() {
-    this.getGeoloc()
+    // this.getGeoloc()
+    let duration
     if (window.location.pathname.toString().includes("/hugs/")) {
-        const duration = 3000
-        return setTimeout(this.getGeoloc.bind(this) , duration);
+        duration = 3000
       } else {
-        const duration = 30000
+        duration = 30000
       }
-      setTimeout(this.getGeoloc.bind(this) , duration);
+    setInterval(this.getGeoloc.bind(this) , duration);
 
   }
 
